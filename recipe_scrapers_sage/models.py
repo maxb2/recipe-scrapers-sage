42422e4c156d837faa057c16bd1e07092840cda0
Matt Anderson <matt@manderscience.com>
2023-07-25 20:26:53 -0500

    wip

--- recipe_scrapers_sage/models.py
+++ recipe_scrapers_sage/models.py
@@ -1,6 +1,7 @@
 """RecipeSage data models."""
 
 from dataclasses import dataclass
+from datetime import timedelta
 from typing import Iterable, Optional, Type
 
 from recipe_scrapers import AbstractScraper
@@ -24,6 +25,14 @@ class Instruction:
     _type: InstructionType
     text: str
 
+    def to_json_ld(self: "Instruction") -> JSON_LD:
+        """Export to JSON-LD.
+
+        Returns:
+            JSON_LD: output
+        """
+        return {"@type": self._type, "text": self.text}
+
 
 @dataclass
 class Comment:
@@ -32,6 +41,18 @@ class Comment:
     name: str
     text: str
 
+    def to_json_ld(self: "Comment") -> JSON_LD:
+        """Export to JSON-LD.
+
+        Returns:
+            JSON_LD: output
+        """
+        return {
+            **COMMENT_CONTEXT,
+            "name": self.name or "Author Notes",
+            "text": self.text,
+        }
+
 
 @dataclass
 class RecipeSage:
@@ -39,17 +60,17 @@ class RecipeSage:
 
     datePublished: Optional[Date]
     description: Optional[str]
-    image: Optional[Iterable[URL]]
+    image: Iterable[URL]
     name: str
     prepTime: Optional[Duration]
     recipeIngredient: Iterable[str]
     recipeInstructions: Iterable[Instruction]
     recipeYield: Optional[str]
     totalTime: Optional[Duration]
-    recipeCategory: Optional[Iterable[str]]
+    recipeCategory: Iterable[str]
     creditText: Optional[str]
     isBasedOn: Optional[URL]
-    comment: Optional[Iterable[Comment]]
+    comment: Iterable[Comment]
 
     @classmethod
     def from_scraper(cls: "Type[RecipeSage]", scraper: AbstractScraper) -> "RecipeSage":
@@ -61,12 +82,65 @@ class RecipeSage:
         Returns:
             RecipeSage: RecipeSage model
         """
-        raise NotImplementedError("This should be implemented.")  # noqa: EM101
 
-    def to_jsonld(self: "RecipeSage") -> JSON_LD:
+        return cls(
+            datePublished=scraper.schema.data.get("datePublished", None),
+            description=scraper.schema.data.get("description", None),
+            image=scraper.schema.image(),
+            name=scraper.schema.data.get("name"),
+            prepTime=scraper.schema.data.get("prepTime", None),
+            recipeIngredient=scraper.schema.ingredients(),
+            recipeInstructions=[
+                Instruction(ins.get("@type", "HowToStep"), ins.get("text", ""))
+                for ins in scraper.schema.data.get("recipeInstructions", [])
+            ],
+            recipeYield=scraper.schema.data.get("recipeYield", None),
+            totalTime=scraper.schema.data.get("totalTime", None),
+            recipeCategory=scraper.schema.data.get("recipeCategory", []),
+            creditText=None,
+            isBasedOn=scraper.url,
+            comment=[],
+        )
+
+    def to_json_ld(self: "RecipeSage") -> JSON_LD:
         """Convert to JSON-LD for import/export.
 
         Returns:
             JSON_LD: JSON-LD data model
         """
-        raise NotImplementedError("This should be implemented.")  # noqa: EM101
+
+        # TODO: refactor this
+        _prepTime: str = ""
+
+        if isinstance(self.prepTime, timedelta):
+            _prepTime = f"PT{self.prepTime.total_seconds()//60}M"
+        elif isinstance(self.prepTime, str):
+            _prepTime = self.prepTime
+
+        _totalTime: str = ""
+
+        if isinstance(self.totalTime, timedelta):
+            _totalTime = f"PT{self.totalTime.total_seconds()//60}M"
+        elif isinstance(self.totalTime, str):
+            _totalTime = self.totalTime
+
+        return {
+            **RECIPE_SAGE_CONTEXT,
+            "datePublished": self.datePublished or "",
+            "description": self.description or "",
+            "image": self.image,
+            "name": self.name,
+            "prepTime": _prepTime,
+            "recipeIngredient": [
+                ingredient for ingredient in self.recipeIngredient if ingredient
+            ],
+            "recipeInstructions": [
+                instruction.to_json_ld() for instruction in self.recipeInstructions
+            ],
+            "recipeYield": self.recipeYield or "",
+            "totalTime": _totalTime,
+            "recipeCategory": self.recipeCategory or [],
+            "creditText": self.creditText or "",
+            "isBasedOn": self.isBasedOn or "",
+            "comment": [comment.to_json_ld() for comment in self.comment],
+        }

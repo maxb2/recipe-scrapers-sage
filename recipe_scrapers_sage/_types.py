a929710dd1f9269f56644fb82f0e09caba625a00
Matt Anderson <matt@manderscience.com>
2023-08-14 13:53:35 -0500

    wip: use TypeAlias

--- recipe_scrapers_sage/_types.py
+++ recipe_scrapers_sage/_types.py
@@ -3,12 +3,22 @@
 from __future__ import annotations
 
 from datetime import datetime, timedelta
-from typing import Literal
+from typing import TYPE_CHECKING, Literal
 
-JSON_LD = dict
-ISO8601_Date = str
-Date = ISO8601_Date | datetime
-URL = str
-ISO8601_Duration = str  # ISO8601Time https://en.wikipedia.org/wiki/ISO_8601#Durations
-Duration = ISO8601_Duration | timedelta
-InstructionType = Literal["HowToSection"] | Literal["HowToStep"]
+if TYPE_CHECKING:
+    import sys
+
+    if sys.version_info > (3, 9):
+        from typing import TypeAlias
+    else:
+        from typing_extensions import TypeAlias
+
+
+JSON_LD: TypeAlias = dict
+ISO8601_Date: TypeAlias = str
+Date: TypeAlias = ISO8601_Date | datetime
+URL: TypeAlias = str
+# ISO8601Time https://en.wikipedia.org/wiki/ISO_8601#Durations
+ISO8601_Duration: TypeAlias = str
+Duration: TypeAlias = ISO8601_Duration | timedelta
+InstructionType: TypeAlias = Literal["HowToSection"] | Literal["HowToStep"]
